a3c4fe4a3258ded9535d19e3dc0fa56c2494a017
Michael Gelfand <michael@senexx.com>
2017-09-27 15:55:12 +0300

    Check if x-pack already installed

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,7 +4,7 @@ set -m
 
 /run/miscellaneous/restore_config.sh
 /run/miscellaneous/edit_config.sh
-/usr/share/kibana/bin/kibana-plugin install x-pack
+test -f /usr/share/kibana/plugins/x-pack/package.json || /usr/share/kibana/bin/kibana-plugin install x-pack
 /run/miscellaneous/wait_for_elasticsearch.sh
 
 # Run as user "logstash" if the command is "kibana"
